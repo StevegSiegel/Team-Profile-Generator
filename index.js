de4e5af6b89c8b10e0af065442d732c18ae38841
Steve Siegel <stevegabrielsiegel@gmail.com>
2021-06-22 20:59:09 -0500

    created rough version of html generator function

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const fs = require('fs');
 //empty array to place team members into//
 let team = [];
 
-function Manager() {
+//start by asking for manager info//
+manager();
+
+// asks for info about the manager//
+function manager() {
     inquirer.prompt([
         {
             type: "input",
@@ -38,17 +42,8 @@ function Manager() {
         });
 }
 
-function addTeam() {
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'teamData',
-            message: 'Would you like to add additional team members?',
-            choices: ['Yes, add and engineer', 'Yes, add an intern', 'No, my team is finished.']
-        }
-    ])
-}
 
+// asks for info about the engineer//
 function engineer() {
     inquirer.prompt([
         {
@@ -79,6 +74,7 @@ function engineer() {
         });
 };
 
+// asks for info about the intern
 function intern() {
     inquirer.prompt([
         {
@@ -100,11 +96,111 @@ function intern() {
 
         .then(function (data) {
             const name = data.name;
-            const id = team.length+1;
+            const id = team.length + 1;
             const email = data.email;
             const school = data.school;
             const newTeamMember = new Intern(name, id, email, school);
             team.push(newTeamMember);
             addTeam();
         });
-}
+};
+
+
+//this is the function that asks if you'd like to add additional team members//
+function addTeam() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'teamData',
+            message: 'Would you like to add additional team members?',
+            choices: ['Yes, add an engineer', 'Yes, add an intern', 'No, my team is finished']
+        }
+    ])
+
+        .then(function (data) {
+            switch (data.teamData) {
+                case "Yes, add an engineer":
+                    engineer();
+                    break;
+
+                case "Yes, add an intern":
+                    intern();
+                    break;
+
+                case "No, my team is finished":
+                    createTeam();
+                    break;
+            }
+
+        });
+};
+
+// adds the info about the employees into an html document//
+function createTeam() {
+    const htmlPage = [];
+    const htmlContent = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <title>Team Profile</title>
+    </head>
+
+    <body>
+        <div class="jumbotron jumbotron-fluid bg-primary bg-gradient text-center pt-4 pb-5">
+          <div class="container">
+            <h1 class="display-4 fw-bold text-white">My Team</h1>
+            </div>
+        </div>
+        `
+    htmlPage.push(htmlContent);
+
+    for (let i = 0; i < team.length; i++) {
+        let card = `
+            <section class="d-flex justify-content-evenly mt-5">
+        
+      <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${team[i].name}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${team[i].role}</h6>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Employee ID: ${team[i].id}</li>
+                <li class="list-group-item">email: ${team[i].email}</li>
+                <li class="list-group-item">office number or github link</li>
+              </ul>
+            </div>
+            `
+        if (team[i].officeNumber) {
+            card += `
+                <li class="list-group-item">${team[i].officeNumber}</li>
+                `
+        }
+
+        if (team[i].github) {
+            card += `
+                <li class="list-group-item">${team[i].github}</li>
+                `
+        }
+
+        if (team[i].school) {
+            card += `
+                <li class="list-group-item">${team[i].school}</li>
+                `
+        }
+
+        card += `
+            </div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+            </body>
+            </html>
+            `
+        htmlPage.push(card)
+    }
+
+        fs.writeFile('Team.html', htmlPage.join(""), function (err){
+
+        })
+
+};
